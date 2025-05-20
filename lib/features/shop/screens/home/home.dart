d750496ecc135aa3610ff4db08563679c011d440
penhele <stephenhelk@gmail.com>
2025-05-20 22:45:47 +0700

    Reusable AppBar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'widgets/home_appbar.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: SingleChildScrollView(
-        child: Column(children: [TPrimaryHeaderContainer(child: Container())]),
+        child: Column(
+          children: [
+            TPrimaryHeaderContainer(
+              child: Column(
+                children: [
+                  // Appbar
+                  THomeAppBar(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
