dfd03dccad9a5166443301df1b0ec52f3c399b4f
penhele <stephenhelk@gmail.com>
2025-05-29 11:49:05 +0700

    Product Detail Screen 2

---
+++ lib/common/widgets/chips/choice_chip.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+import '../../../utils/constants/colors.dart';
+import '../../../utils/helpers/helper_functions.dart';
+import '../custom_shapes/containers/circular_container.dart';
+
+class TChoiceChip extends StatelessWidget {
+  const TChoiceChip({
+    super.key,
+    required this.text,
+    required this.selected,
+    this.onSelected,
+  });
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
+
+  @override
+  Widget build(BuildContext context) {
+    final isColor = THelperFunctions.getColor(text) != null;
+
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor ? const SizedBox() : Text(text),
+        labelStyle: TextStyle(color: selected ? TColors.white : null),
+        labelPadding: isColor ? const EdgeInsets.all(0) : null,
+        selected: selected,
+        onSelected: onSelected,
+        avatar:
+            isColor
+                ? TCircularContainer(
+                  width: 50,
+                  height: 50,
+                  backgroundColor: THelperFunctions.getColor(text)!,
+                )
+                : null,
+        padding: isColor ? const EdgeInsets.all(0) : null,
+        shape: isColor ? const CircleBorder() : null,
+        backgroundColor: isColor ? THelperFunctions.getColor(text)! : null,
+      ),
+    );
+  }
+}
