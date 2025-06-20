7c4dc44aa15be0ed6aff02d5a92636dede2628b9
penhele <stephenhelk@gmail.com>
2025-06-21 00:23:39 +0700

    Onboarding Screen Only One Time

--- lib/data/repositories/authentication/authentication_repository.dart
+++ lib/data/repositories/authentication/authentication_repository.dart
@@ -1,8 +1,9 @@
 import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
-import 'package:t_store/features/authentication/screens/login/login.dart';
-import 'package:t_store/features/authentication/screens/onboarding/onboarding.dart';
+
+import '../../../features/authentication/screens/login/login.dart';
+import '../../../features/authentication/screens/onboarding/onboarding.dart';
 
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
